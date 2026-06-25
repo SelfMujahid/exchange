f9c49dd6dd344e9bf3a3c0f0153a5324faa03837
SelfMujahid <137253456+SelfMujahid@users.noreply.github.com>
2026-06-25 22:48:02 +0500

    Delete app/build.gradle.kts

--- app/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-dependencies {
-    // OkHttp for ultra-low latency WebSockets & REST API
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    // Coroutines for background threading
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    // Gson for JSON parsing
-    implementation("com.google.code.gson:gson:2.10.1")
-}
