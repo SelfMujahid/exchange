df68fc79225ef7a60e900168c4e4d948d6651962
SelfMujahid <137253456+SelfMujahid@users.noreply.github.com>
2026-06-28 00:13:56 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,28 @@
-plugin {
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.practice.cryptoapp"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFiles("proguard-android-optimize.txt"),"proguard-rules.pro")
+        }
+    }
+
+    complieOptions {
+        sourceCompatibility = javaVersion.VERSION_1_8
+        targetCompatibility = javaVersion.VERSION_1_8
+    }
